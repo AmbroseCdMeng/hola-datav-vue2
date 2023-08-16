ef1c02d2714eaae1777479e68594fcb8f8389761
AmbroseCdmeng <mcd.951026@foxmail.com>
2023-08-16 14:30:58 +0800

    init

---
+++ src/components/dashboard/SecurityScreen.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="height:640px;">
+    <dv-border-box-1></dv-border-box-1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SecurityScreen',
+  props: {
+    msg: String
+  }
+}
+</script>
